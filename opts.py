c8bd3533ab7222806c1454d9576db9ea1b63d529
jrmkim50 <jrmkim50@gmail.com>
2022-02-01 12:28:47 -0800

    modified opts

--- opts.py
+++ opts.py
@@ -19,7 +19,7 @@ class Opts:
         self.parser_prepare.add_argument('--glob_ext', default='*.nii.gz', help='Extension of files for globbing')
         self.parser_prepare.add_argument('--n_img_per_shard', default=500, type=int, help='No. of images per shard')
         self.parser_prepare.add_argument('--start_resolution', default=4, type=int, help='Start resolution for generator')
-        self.parser_prepare.add_argument('--target_resolution', default=256, type=int, help='Target resolution for generator')
+        self.parser_prepare.add_argument('--target_resolution', default=128, type=int, help='Target resolution for generator')
 
         # Train Task
         self.parser_train = self.subparsers.add_parser('train', help='Train the progressive GAN')
@@ -36,10 +36,10 @@ class Opts:
         self.parser_train.add_argument('--num_channels', default=1, type=int, help='Number of channels in images')
         self.parser_train.add_argument('--num_classes', default=1, type=int, help='Number of classes (only 1 supported')
 
-        self.parser_train.add_argument('--kiters_per_transition', default=0, type=float, help='x*1000 iterations per transition, leave it zero to use resolution specific iters')
-        self.parser_train.add_argument('--kiters_per_resolution', default=0, type=float, help='x*1000 iterations per resolution, leave it zero to use resolution specific iters')
+        self.parser_train.add_argument('--kiters_per_transition', default=10, type=float, help='x*1000 iterations per transition, leave it zero to use resolution specific iters')
+        self.parser_train.add_argument('--kiters_per_resolution', default=10, type=float, help='x*1000 iterations per resolution, leave it zero to use resolution specific iters')
         self.parser_train.add_argument('--start_resolution', default=4, type=int, help='start resolution')
-        self.parser_train.add_argument('--target_resolution', default=256, type=int, help='target resolution')
+        self.parser_train.add_argument('--target_resolution', default=128, type=int, help='target resolution')
         # self.parser_train.add_argument('--resolution_batch_size')
         self.parser_train.add_argument('--d_repeats', default=1, type=int, help='Batches of discriminator per generator batch')
 
@@ -91,7 +91,7 @@ class Opts:
             config.img_ext = 'jpg' if config.dimensionality == 2 else 'nii.gz'
 
             # config.resolution_batch_size = {4: 64, 8: 32, 16: 16, 32: 8, 64: 4, 128: 2, 256: 1} # per gpu
-            config.resolution_batch_size = {4: 64, 8: 64, 16: 32, 32: 32, 64: 16, 128: 2, 256: 1} # per gpu
+            config.resolution_batch_size = {4: 1024, 8: 512, 16: 128, 32: 32, 64: 8, 128: 8} # per gpu
 
             if config.kiters_per_transition == 0:
                 config.kiters_per_transition = {4: 80, 8: 100, 16: 120, 32: 140, 64: 160, 128: 180, 256: 200}
